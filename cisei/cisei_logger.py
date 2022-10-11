3f4881a19a881f879f485aa874d92f687d2aba1a
Franek Borowiec <franek.borowiec@gmail.com>
2022-10-11 12:43:54 +0200

    Cisei names logger

---
+++ cisei/cisei_logger.py
@@ -0,0 +1,67 @@
+import psycopg2
+from psycopg2.extras import Json
+
+
+class LoggerDB:
+    HOST: str = "database_pg"
+    PORT: str = "5432"
+    DB_NAME: str = "database_pg"
+    USER: str = "postgres"
+    PASSWORD: str = "postgres"
+
+    def __init__(self) -> None:
+        self.conn = psycopg2.connect(
+            host=self.HOST,
+            database=self.DB_NAME,
+            port=self.PORT,
+            user=self.USER,
+            password=self.PASSWORD,
+        )
+        self.c = self.conn.cursor()
+
+        self.create_person_info_table()
+
+    def query_post(self, path: str, **kwargs) -> None:
+        with open(path, "r", encoding="utf-8") as query_file:
+            query: str = query_file.read()
+            with self.db_connection:
+                self.cursor.execute(query, kwargs)
+                self.db_connection.commit()
+
+    def query_fetch_one(self, path: str) -> tuple:
+        with open(path, "r", encoding="utf-8") as query_file:
+            query: str = query_file.read()
+            with self.db_connection:
+                self.cursor.execute(query)
+                return self.cursor.fetchone()
+
+    def query_fetch_all(self, path: str, **kwargs) -> list:
+        with open(path, "r", encoding="utf-8") as query_file:
+            query: str = query_file.read()
+            with self.db_connection:
+                self.cursor.execute(query, kwargs)
+                return self.cursor.fetchall()
+
+    def create_person_info_table(self):
+        self.query_post("database/queries/create_person_info_table.sql")
+
+    def add_person_info(self, person_info) -> None:
+        self.query_post(
+            "database/queries/add_person_info.sql",
+            person_info.idx,
+            person_info.surname,
+            person_info.full_name,
+            person_info.age,
+            person_info.trip_date,
+            person_info.registration_place,
+            person_info.url,
+            Json(person_info.details),
+        )
+
+    def display_person_info(self):
+        self.query_fetch_all("database/queries/display_person_info.sql")
+
+    def __del__(self) -> None:
+        self.conn.commit()
+        self.c.close()
+        self.conn.close()
